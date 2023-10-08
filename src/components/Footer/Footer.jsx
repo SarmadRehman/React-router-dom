dc30ee62c80a92b980138099115ece1bb9621149
Sarmad Rehman <sarmadr02@gmail.com>
2023-10-08 06:17:35 -0700

    components updated a-bit , new routing style

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -38,7 +38,7 @@ export default function Footer() {
               <ul className="text-gray-500 font-medium">
                 <li className="mb-4">
                   <a
-                    href="https://github.com/hiteshchoudhary"
+                    href="https://github.com/SarmadRehman"
                     className="hover:underline"
                     target="_blank"
                     rel="noreferrer"
@@ -76,8 +76,11 @@ export default function Footer() {
         <div className="sm:flex sm:items-center sm:justify-between">
           <span className="text-sm text-gray-500 sm:text-center">
             © 2023
-            <a href="https://hiteshchoudhary.com/" className="hover:underline">
-              hiteshchoudhary
+            <a
+              href="https://www.linkedin.com/in/sarmad-rehman-bb450b1bb/"
+              className="hover:underline"
+            >
+              SarmadRehman
             </a>
             . All Rights Reserved.
           </span>
--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,42 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import Home from "./components/Home/Home.jsx";
 import About from "./components/About/About.jsx";
+import Contact from "./components/Contact/Contact.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      { path: "", element: <Home /> },
-      { path: "about", element: <About /> },
-    ],
-  },
-]);
+// there are certain ways to write these routes, one is this
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     children: [
+//       { path: "", element: <Home /> },
+//       { path: "about", element: <About /> },
+//       { path: "contact", element: <Contact /> },
+//     ],
+//   },
+// ]);
+
+// another way to define routing
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      {/* <Route path="user/:userid" element={<User />} /> */}
+      {/* <Route loader={githubInfoLoader} path="github" element={<Github />} /> */}
+    </Route>
+  )
+);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
